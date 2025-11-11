07c1bac661ad938f6d0ba42f3bd6682dbe62c950
Brian Lee <leebrian0908@gmail.com>
2025-11-11 19:06:24 +0800

    Initial Commit

---
+++ Pig Dice Game/Pig_Dice_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pig_Dice_GameApp.swift
+//  Pig Dice Game
+//
+//  Created by Brian Lee on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Pig_Dice_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
